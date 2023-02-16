0d96213923c01fe9b978715b46aff90995f67ef0
Chris <petermanchristoph@gmail.com>
2023-02-16 16:58:22 -0500

    Added solution for example problem

--- src/main/java/com/advent/service/DayOne.java
+++ src/main/java/com/advent/service/DayOne.java
@@ -1,5 +1,10 @@
 package com.advent.service;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.advent.util.AdventPaths;
 import com.advent.util.DataGetter;
 import com.advent.util.DriverLoader;
@@ -14,11 +19,39 @@ public class DayOne implements Solution{
 		this.driverLoader = driverLoader;
 		this.dataGetter = dataGetter;
 	}
+	
+	private int findMax(Map<Integer, Integer> map) {
+		return Collections.max(map.values());
+	}
+	
+	private Map<Integer, Integer> convertToMap(String[] data) {
+		Map<Integer, Integer> map = new HashMap<>();
+		int elf = 0;
+		int total = 0;
+		for(String number: data) {
+			if(number.trim().equals("")) {
+				map.put(elf, total);
+				elf++;
+				total = 0;
+			} else {
+				total += Integer.parseInt(number.trim());
+			}
+		}
+		
+		return map;
+	}
+	
+	private String[] splitData (String data){
+		return data.split("\n");
+	}
 
 	@Override
 	public int solveExample() {
-		
-		return 0;
+		driverLoader.getUrl(paths.getPathToDayProblem());
+		String data = dataGetter.getExampleData(driverLoader.getDriver(), paths.getPathToExampleData());
+		String[] splitData = splitData(data);
+		Map<Integer, Integer> map = convertToMap(splitData);
+		return findMax(map);
 	}
 
 	@Override
--- src/test/java/com/advent/daytests/DayOneTest.java
+++ src/test/java/com/advent/daytests/DayOneTest.java
@@ -2,17 +2,56 @@ package com.advent.daytests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import com.advent.exceptions.InvalidDriverException;
 import com.advent.service.DayOne;
+import com.advent.util.AdventPaths;
+import com.advent.util.DataGetter;
+import com.advent.util.DriverLoader;
 
-@TestInstance(Lifecycle.PER_CLASS)
 class DayOneTest {
+	private DayOne dayOne;
+	private AdventPaths paths;
+	private DataGetter dataGetter;
+	private DriverLoader driverLoader;
+	
+	private String pathToDayProblem = "https://adventofcode.com/2022/day/1";
+	private String pathToExampleData = "/html/body/main/article/pre/code";
+	private String pathToOpenProblemData = "/html/body/main/p[1]/a";
+	private String pathToProblemData = "/html/body/pre";
+	private String pathToSubmissionInput = "/html/body/main/form/p/input[1]";
+	private String pathToSubmit = "/html/body/main/form/p/input[2]";
+	
+	@BeforeEach
+	void setUp() throws InvalidDriverException {
+		driverLoader = new DriverLoader();
+		driverLoader.loadDriver("Chrome");
+		paths = new AdventPaths(pathToDayProblem, pathToExampleData, pathToOpenProblemData, pathToProblemData, pathToSubmissionInput, pathToSubmit);
+		dataGetter = new DataGetter();
+		
+		dayOne = new DayOne(paths, driverLoader, dataGetter);
+	}
+
+	@Test
+	void test() {
+		int result = dayOne.solveExample();
+		int expected = 24000;
+		assertEquals(expected, result);
+	}
+	
+	@AfterEach
+	void tearDown() {
+		if(driverLoader.getDriver() != null) {
+			driverLoader.closeDriver();
+		}
+		
+		driverLoader = null;
+		paths = null;
+		dataGetter = null;
+		dayOne = null;
+	}
 
 }
